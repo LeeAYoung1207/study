20f20ed0ffbbe505823044c9565a0a9e5ad1963f
LeeAYoung1207 <elluan3369@gmail.com>
2023-04-10 22:22:19 +0900

    member repository 추가

---
+++ src/main/java/study/study1/repository/Member2Repository.java
@@ -0,0 +1,7 @@
+package study.study1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.study1.entity.Member2;
+
+public interface Member2Repository extends JpaRepository<Member2, String> {
+}
